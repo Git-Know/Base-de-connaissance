53ea8645b9c641fa8eb5f69787d1388843809f4e
naderite <bsc.gg03@gmail.com>
2025-08-07 11:58:24 +0100

    feat: automated the data ingestion pipeline and added Docker support for the recommendation API

--- start_all.py
+++ start_all.py
@@ -3,12 +3,12 @@ import time
 from kafka import KafkaProducer
 import sys
 
-DOCKER_COMPOSE_CMD = ["docker","compose", "up", "-d"]
+DOCKER_COMPOSE_CMD = ["docker","compose", "up", "--build","-d"]
 KAFKA_BOOTSTRAP_SERVERS = "localhost:9092"
 PRODUCER_SCRIPT = "producer.py"
 CONSUMER_SCRIPT = "consumer.py"
 KAFKA_TIMEOUT = 60  
-
+NEO4J_CONSUMER_SCRIPT = "neo4j_kafka_consumer.py"
 
 def start_docker():
     print("🛠️  Démarrage des services Docker...")
@@ -35,11 +35,27 @@ def wait_for_kafka(timeout=KAFKA_TIMEOUT):
                 sys.exit(1)
             time.sleep(2)
 
+def run_spark_job():
+    print("🔥 Lancement du job Spark Datacleaning.py dans le conteneur Spark...")
+    try:
+        subprocess.run( [
+    "docker", "exec",
+    "--user", "root",           # ← run as root so Hadoop/Spark can resolve the user
+    "spark-master",
+    "/opt/bitnami/spark/bin/spark-submit",
+    "--master", "local[*]",
+    "--conf", "spark.jars.ivy=/tmp/.ivy2",
+    "--packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0",
+    "/opt/bitnami/spark/work/Datacleaning.py"
+]    , check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"❌ Erreur lors de l'exécution du job Spark : {e}")
+        sys.exit(1)
 
 def run_producer_script():
     print("🚀 Lancement du script producer.py...")
     try:
-        subprocess.run(["py", PRODUCER_SCRIPT], check=True)
+        subprocess.run([sys.executable, PRODUCER_SCRIPT], check=True)
     except subprocess.CalledProcessError as e:
         print(f"❌ Erreur lors de l'exécution de {PRODUCER_SCRIPT} : {e}")
         sys.exit(1)
@@ -47,13 +63,22 @@ def run_producer_script():
 def run_consumer_script():
     print("🚀 Lancement du script consumer.py...")
     try:
-        subprocess.run(["py", CONSUMER_SCRIPT], check=True)
+        subprocess.run([sys.executable, CONSUMER_SCRIPT], check=True)
     except subprocess.CalledProcessError as e:
         print(f"❌ Erreur lors de l'exécution de {CONSUMER_SCRIPT} : {e}")
         sys.exit(1)
-
+def run_neo4j_consumer_script():
+    print("🚀 Lancement du script ne4j kafa consumer.py...")
+    try:
+        subprocess.run([sys.executable, NEO4J_CONSUMER_SCRIPT], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"❌ Erreur lors de l'exécution de {NEO4J_CONSUMER_SCRIPT} : {e}")
+        sys.exit(1)
+    
 if __name__ == "__main__":
     start_docker()
     wait_for_kafka()
     run_producer_script()
+    run_spark_job()
+    run_neo4j_consumer_script()
     run_consumer_script()
