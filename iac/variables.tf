2f8f5dd4b616473891c6f077de95f5619ec8c7d2
meryem boukraa <meryem.boukraa@esprit.tn>
2025-08-12 13:47:24 +0100

    new fix

---
+++ iac/variables.tf
@@ -0,0 +1,21 @@
+# variables.tf
+
+variable "aws_region" {
+  type    = string
+  default = "eu-west-3" # Paris
+}
+
+variable "ami_id" {
+  type    = string
+  default = "ami-0c02fb55956c7d316" # Ubuntu 20.04 (update if needed)
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "key_name" {
+  type = string
+  description = "The name of your AWS key pair"
+}
