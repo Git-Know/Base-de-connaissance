1701a00cf355e7ff5f257aa39f543339969b29e6
naderite <bsc.gg03@gmail.com>
2025-08-14 12:00:10 +0100

    fix: changed aws region and removed localstack from docker compose and changed config vraiables and credentials to enviroment variables

--- IAC/main.tf
+++ IAC/main.tf
@@ -1,13 +1,34 @@
 provider "aws" {
-  region                      = "us-east-1"
-  access_key                  = "test"
-  secret_key                  = "test"
+  region                      = "eu-west-3"
+  access_key                  = var.aws_access_key
+  secret_key                  = var.aws_secret_key
   s3_force_path_style         = true
   skip_credentials_validation = true
   skip_metadata_api_check     = true
   skip_requesting_account_id  = true
+
   endpoints {
-    ec2 = "http://localhost:4566"
-    s3  = "http://localhost:4566"
+    ec2 = var.ec2_endpoint
+    s3  = var.s3_endpoint
   }
+}
+
+variable "aws_access_key" {
+  type        = string
+  description = "AWS Access Key"
+}
+
+variable "aws_secret_key" {
+  type        = string
+  description = "AWS Secret Key"
+}
+
+variable "ec2_endpoint" {
+  type        = string
+  description = "Custom EC2 endpoint URL"
+}
+
+variable "s3_endpoint" {
+  type        = string
+  description = "Custom S3 endpoint URL"
 }
--- IAC/networking.tf
+++ IAC/networking.tf
@@ -11,7 +11,7 @@ resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-east-1a"
+  availability_zone       = "eu-west-3a"
 
   tags = {
     Name = "public-subnet"
@@ -22,7 +22,7 @@ resource "aws_subnet" "public" {
 resource "aws_subnet" "private" {
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.2.0/24"
-  availability_zone = "us-east-1a"
+  availability_zone = "eu-west-3a"
 
   tags = {
     Name = "private-subnet"
